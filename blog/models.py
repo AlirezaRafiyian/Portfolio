fa6ed00556fc45f31235abb1fe54eae6219a91e7
alex7hss <alex7hss@gmail.com>
2021-08-27 14:33:56 +0430

    firstcommit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pubDate = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100] + ' .....'
+
+    def pubDatePretty(self):
+        return self.pubDate.strftime('%b %e %Y')
